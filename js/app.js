5e46c2224dc6cb8fb08f9ebeda7c49c8f1b261b5
Yeah.not <yeahhh.not@gmail.com>
2020-10-22 12:25:22 +0300

    Добавил AJAX обработку формы заказа с выводом информации и поведением попапа.

--- js/app.js
+++ js/app.js
@@ -5,14 +5,13 @@ $(function() {
   $('body').toggleClass('no-js js');
 
   let $mainNav = $('.main-nav');
+  let addToCartPopup = $('.add-to-cart').popup({closeBtn: false, overlay: true})[0];
+  let contactUsPopup = $('.contact-us').popup()[0];
 
   $('.main-nav__toggle').on('click', function(evt) {
     evt.preventDefault();
     $mainNav.toggleClass('main-nav--closed main-nav--opened');
   });
-
-  let addToCartPopup = $('.add-to-cart').popup({closeBtn: false, overlay: true})[0];
-  let contactUsPopup = $('.contact-us').popup()[0];
 
   $('.js-add-to-cart-show').on('click', function(evt) {
     evt.preventDefault();
@@ -22,5 +21,30 @@ $(function() {
   $('.js-contact-us-show').on('click', function(evt) {
     evt.preventDefault();
     contactUsPopup.open();
+  });
+
+  $('.add-to-cart__form').on('submit', function(evt) {
+    evt.preventDefault();
+
+    let form = this;
+    let $form = $(this);
+    let $response = $form.find('.add-to-cart__response');
+    errorClass = 'add-to-cart__response--error';
+
+    $.post(form.action, $form.serialize(), function(data){
+      if (data === 'Совсем нет никаких данных :(') {
+        $response.addClass(errorClass);
+      } else {
+        $response.removeClass(errorClass);
+
+        setTimeout(function() {
+          form.reset();
+          $response.empty();
+          addToCartPopup.close();
+        }, 2000);
+      }
+
+      $response.html(data);
+    });
   });
 })
